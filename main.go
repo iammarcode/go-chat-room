39daa332e99d8b90539a56e09c937cc7a4290ad1
Marco <marcoonway@gmail.com>
2022-04-11 14:44:43 +0800

    remove runtime folder

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/whoismarcode/go-chat-room/global"
 	"github.com/whoismarcode/go-chat-room/initialize"
 )
 
 func main() {
 	initialize.LoadConfig("./")
-
-	// test config
-	fmt.Println(global.Config.App.RuntimeRootPath)
 }
